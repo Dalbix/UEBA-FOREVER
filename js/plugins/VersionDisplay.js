666ba29b2fe55e94e67af208ca139058043a6348
Dalbix <the3dalbix@hotmail.com>
2025-11-17 17:08:23 +0100

    Auto commit 17/11/2025 17:08:22,91

--- js/plugins/VersionDisplay.js
+++ js/plugins/VersionDisplay.js
@@ -22,7 +22,7 @@
         }
     } else {
         // --- Código para web ---
-        versionText = "v.20251115.0352"; // Puedes cambiar por algo dinámico si quieres
+        versionText = "v.20251117.0352"; // Puedes cambiar por algo dinámico si quieres
     }
 
     // --- En la pantalla de título ---
