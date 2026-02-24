40d776050ecb13bbf9e79331799bf37156a3a7e3
Dalbix <the3dalbix@hotmail.com>
2026-02-25 00:08:57 +0100

    Auto commit 25/02/2026  0:08:57,18

--- js/plugins/VersionDisplay.js
+++ js/plugins/VersionDisplay.js
@@ -22,7 +22,7 @@
         }
     } else {
         // --- Código para web ---
-        versionText = "v.1.26.2-14"; // Puedes cambiar por algo dinámico si quieres
+        versionText = "v.1.26.2-24"; // Puedes cambiar por algo dinámico si quieres
     }
 
     // --- En la pantalla de título ---
